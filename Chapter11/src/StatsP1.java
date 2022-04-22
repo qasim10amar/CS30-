ebc1def9890e5a3836e8d3b0de5d176bcc298d7a
54150001 <54150001@L815WD220006.edu.cbe.ab.ca>
2022-04-22 10:06:52 -0600

    Draft

--- Chapter11/src/StatsP1.java
+++ Chapter11/src/StatsP1.java
@@ -11,6 +11,7 @@ Course: Computer Programming 30
 */
 
 import java.io.*;
+import java.text.NumberFormat;
 
 public class StatsP1 {
 
@@ -18,44 +19,66 @@ public class StatsP1 {
 		public static void main(String[] args) {
 			
 		
-			 File dataFile= new File("scores. dat");
-			 FileReader in;
-			 BufferedReader readFile;
-			 String score;
-			 double avgScore;
-			 double totalScores = 0;
-			 int numScores = 0;
+			double avgScore;
+			double totalScores = 0;
+			int numScores = 0;
+			String name;
+			String score;
+			NumberFormat percent = NumberFormat.getPercentInstance();
+			File dataFile= new File("test1.dat");
 
-			 try {
 
-			 in = new FileReader(dataFile);
-			 readFile = new BufferedReader(in);
-			 while ((score= readFile.readLine ()) != null ) {
-			 numScores += 1;
-			 System.out.println(score);
-			 totalScores += Double.parseDouble (score);
-			 }
-			 avgScore = totalScores / numScores;
-			 System. out.println("Average =" + avgScore);
-			 readFile.close();
-			 in.close();
-			 }
-			 catch (FileNotFoundException e)
-			 {
-			 System. out. println("File does not exist or could not be found.");
-			 System.err.println("FileNotFoundException:" + e. getMessage ());
-			 }
-			 catch (I0Exception e)
-			 {
-			 System.out.println("Problem reading file.");
-			 System.err.println("I0Exception: "+ e.getMessage());
+			FileReader in;
+			BufferedReader readFile;
 
-			 }
-	}
+			try {
+			
+			in = new FileReader(dataFile);
+			readFile = new BufferedReader(in);
+			name=readFile.readLine();
+			System.out.println(name);
+			   
+			while ((score = readFile.readLine()) != null) {
+			totalScores+= Double.parseDouble(score);
+			numScores+=1;
+			System.out.println(score);
+			   
+			}
+			
+		    avgScore = totalScores/numScores;
+		    System.out.print("\n" + name + " Average = " + percent.format(avgScore/100));
 
-}
+			readFile.close();
+			in.close();
+			
+			} 
+			
+			catch (FileNotFoundException e) {
+			System.out.println("File does not exist or could not be found.");
+			System.err.println("FileNotFoundException: " + e.getMessage());
+			
+			} 
+			
+			catch (IOException e) {
+			System.out.println("Problem reading file.");
+			System.err.println("IOException: " + e.getMessage());
+			
+			}
+
+			}
+		}
+
 
 
 /* Screen Dump
+ 
+Qasim Amar
+43
+100
+56
+78
+93
+
+Qasim Amar Average = 74%
  
 */
