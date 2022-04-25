1748b35ea2b98151cb91a6d93d6834fef56d5f50
54150001 <54150001@L815WD220005.edu.cbe.ab.ca>
2022-04-25 11:58:37 -0600

    Draft

--- Chapter11/src/StatsP2.java
+++ Chapter11/src/StatsP2.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 /*
 
 Program: StatsP2.java          Last Date of this Revision: Apr 11, 2022
@@ -17,7 +22,33 @@ public class StatsP2 {
 
 	public static void main(String[] args) {
 	
+		File dataFile = new File("StuScores.dat");
+		FileWriter out;
+		BufferedWriter = writeFile;
+		Scanner input = new Scanner(System.in);
+		double score;
+		String name;
 
+		try {
+			out = new FileWriter(dateFile);
+			writeFile = new BufferedWriter(out);
+			for (int i = 0; i < 5; i++) {
+				System.out.print("Enter student name: ");
+				name = input.next();
+				System.out.print("Enter test score: ");
+				score = input.nextDouble();
+				writeFile.write(name);
+				writeFile.newLine();
+				writeFile.write(String.valueOf(score));
+				writeFile.newLine();
+			}
+			writeFile.close();
+			out.close();
+			System.out.println("Data written to file.");
+		}   catch (IOException e) {
+			System.out.println("Problem writing to file.");
+			System.err.println("IOException: " + e.getMessage());
+		}
 	}
 
 }
