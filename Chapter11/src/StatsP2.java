2923242c68aa4645321296b43be62ed3885dde36
hamzaamar <hamzaamar@192.168.1.72>
2022-05-06 21:47:12 -0600

    Draft

--- Chapter11/src/StatsP2.java
+++ Chapter11/src/StatsP2.java
@@ -8,11 +8,11 @@ import java.util.Scanner;
 
 /*
 
-Program: StatsP2.java          Last Date of this Revision: Apr 11, 2022
+Program: StatsP2.java          Last Date of this Revision: Apr 14, 2022
 
-Purpose: 
+Purpose:
 
-Author: Qasim Amar, 
+Author: Qasim Amar,
 School: CHHS
 Course: Computer Programming 30
  
@@ -23,102 +23,110 @@ Course: Computer Programming 30
 public class StatsP2 {
 
 
-	public static void main(String[] args) {
-	
-		Scanner input = new Scanner(System.in);
-		FileWriter out;
-		BufferedWriter writeFile;
-		String Name;
-		Double Score;
-		int NumScore;	
-		double Avg;
-		double[] Grades;
-		double lowest;
-		double highest;
-		double TotScore = 0;
-		NumberFormat percent = NumberFormat.getPercentInstance();
-		
+public static void main(String[] args) {
 
-		File dataFile = new File("GradeReport.txt");
+Scanner input = new Scanner(System.in);
+FileWriter out;
+BufferedWriter writeFile;
+String Name;
+Double Score;
+int NumScore;
+double Avg;
+double[] Grades;
+double lowest;
+double highest;
+double TotScore = 0;
+String fileName;
+File dataFile;
+NumberFormat percent = NumberFormat.getPercentInstance();
 
-		try {
-		out = new FileWriter(dataFile);
-		writeFile = new BufferedWriter(out);
 
-		System.out.println("Enter your Name : ");
-		Name=input.nextLine();
-		writeFile.write(Name);
-		writeFile.newLine();
+System.out.println("Enter the name of the file: ");
+fileName=input.next();
+dataFile = new File(fileName);
+input.nextLine();
 
-		System.out.println("How many Scores would you Like to Enter? : ");
-		NumScore = input.nextInt();
-		Grades = new double[NumScore];
+try {
+out = new FileWriter(dataFile);
+writeFile = new BufferedWriter(out);
 
-		System.out.println("Enter " + NumScore + " class Score: ");
+System.out.println("Enter your Name : ");
+Name=input.nextLine();
+writeFile.write(Name);
+writeFile.newLine();
 
-		for( int i =0; i<NumScore; i++) {
-		Score=input.nextDouble();
-		TotScore += Score;
-		Grades[i] = Score;
-		writeFile.write(String.valueOf(Score));
-		writeFile.newLine();
-		}
+System.out.println("How many Scores would you Like to Enter? : ");
+NumScore = input.nextInt();
+Grades = new double[NumScore];
 
-		Avg = TotScore/NumScore;
+System.out.println("Enter " + NumScore + " class Score: ");
 
-		    highest =Grades[0];
-		    lowest=Grades[0] ;
-		   
-		    for (int i = 1; i < Grades.length; i++) 
-		    {
-		    if (Grades[i] > highest)
-		    highest = Grades[i];
-		    else if (Grades[i] < lowest)
-		    lowest = Grades[i];
-		    }
-		   
-		    System.out.print( "\n" + Name + "\n" + "Average = " + percent.format(Avg/100)
-		    + "\n" + "Lowest Score :" + lowest + "\n" + "Highest Score: " + highest);
-		   
-		    writeFile.close();
-		    out.close();
-		    } 
-		
-		catch (FileNotFoundException e) {
-			
-		System.out.println("File does not exist or could not be found.");
-		System.err.println("FileNotFoundException: " + e.getMessage());
-		
-		} 
-		catch (IOException e) {
-		System.out.println("Problem reading file.");
-		System.err.println("IOException: " + e.getMessage());
-		
-		   }
-		
-		}
+for( int i =0; i<NumScore; i++) {
+Score=input.nextDouble();
+TotScore += Score;
+Grades[i] = Score;
+writeFile.write(String.valueOf(Score));
+writeFile.newLine();
+}
 
-		}
+Avg = TotScore/NumScore;
 
-		
+   highest =Grades[0];
+   lowest=Grades[0] ;
+ 
+   for (int i = 1; i < Grades.length; i++)
+   {
+   if (Grades[i] > highest)
+   highest = Grades[i];
+   else if (Grades[i] < lowest)
+   lowest = Grades[i];
+   }
+ 
+   System.out.print( "\n" + "Name: " + Name + "\n" + "Average = " + percent.format(Avg/100)
+   + "\n" + "Lowest Score :" + percent.format(lowest/100) + "\n" + "Highest Score: " + percent.format(highest/100));
+ 
+   writeFile.close();
+   out.close();
+   }
+
+catch (FileNotFoundException e) {
+
+System.out.println("File does not exist or could not be found.");
+System.err.println("FileNotFoundException: " + e.getMessage());
+
+}
+catch (IOException e) {
+System.out.println("Problem reading file.");
+System.err.println("IOException: " + e.getMessage());
+
+  }
+
+}
+
+}
+
+
 
 /* Screen Dump
  
-Enter your Name : 
+Enter the name of the file:
+Marks
+Enter your Name :
 Qasim Amar
-How many Scores would you Like to Enter? : 
+How many Scores would you Like to Enter? :
 5
-Enter 5 class Score: 
+Enter 5 class Score:
 100
-95
-78
-67
+92
 83
+74
+87
 
-Qasim Amar
-Average = 85%
-Lowest Score :67.0
-Highest Score: 100.0
+Name: Qasim Amar
+Average = 87%
+Lowest Score :74%
+Highest Score: 100%
  
 */
+
  
