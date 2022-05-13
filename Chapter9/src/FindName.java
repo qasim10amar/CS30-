63f12c5c4e8d20fc0a5a07498aa9057c21973e9b
54150001 <54150001@L815WD220006.edu.cbe.ab.ca>
2022-05-13 10:26:50 -0600

    Draft

---
+++ Chapter9/src/FindName.java
@@ -0,0 +1,9 @@
+
+public class FindName {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ Chapter9/src/Squares.java
@@ -0,0 +1,33 @@
+/*
+
+Program: Squares.java          Last Date of this Revision: May 10, 2022
+
+Purpose: Create a squares application that stores the square of an elements's in an integer array of 5 elements.
+
+Author: Qasim Amar, 
+School: CHHS
+Course: Computer Programming 30
+ 
+*/
+
+public class Squares {
+
+	public static void main(String[] args) {
+		
+		//Creates a new integer array with 5 elements
+		int[] array = new int[5];
+
+		//For loop that runs the length of the array number of times
+		for (int i=0; i<array.length; i++) {
+			
+		//Stores the square of the index for each element starting from 0 to 4
+		array[i]=(i * i);
+		
+		//Prints each elements of the array in order
+		System.out.println(array[i]);
+		
+
+	}
+
+}
+}
