4aed4e78a3e864d3e8c1cf9d7a810057ce5b869f
54150001 <54150001@L815WD220006.edu.cbe.ab.ca>
2022-06-08 11:53:44 -0600

    Draft

---
+++ PhidgetRover/src/Move.java
@@ -0,0 +1,52 @@
+/*
+
+Program: Move.java          Last Date of this Revision: Jun 8, 2022
+Purpose: Learn to move rover forward and backwards.
+
+Author: Qasim Amar
+School: CHHS
+Course: CS30
+
+*/
+
+//Add Phidgets Library
+import com.phidget22.*;
+
+public class Move {
+  public static void main(String[] args) throws Exception {
+
+      //Connect to wireless rover
+      Net.addServer("", "192.168.100.1", 5661, "", 0);
+
+      //Create
+      DCMotor leftMotors = new DCMotor();
+      DCMotor rightMotors = new DCMotor();
+
+      //Address
+      leftMotors.setChannel(0);
+      rightMotors.setChannel(1);
+
+      //Open
+      leftMotors.open(5000);
+      rightMotors.open(5000);
+
+      //Move forward at full speed
+      leftMotors.setTargetVelocity(-1);
+      rightMotors.setTargetVelocity(-1);
+     
+      /*
+      //Move backwards at full speed
+      leftMotors.setTargetVelocity(1);
+      rightMotors.setTargetVelocity(1);
+ 	  */
+
+      //Wait for 1 second
+      Thread.sleep(1000);
+     
+     
+
+      //Stop motors
+      leftMotors.setTargetVelocity(0);
+      rightMotors.setTargetVelocity(0);
+  }
+}
